83b22963928b287bfb04a9ddae7138706ac650e1
YoungDatsun <nvzedbazi2@gmail.com>
2024-12-29 14:43:28 +0330

    Buttons,Cards,Br

---
+++ index.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.card button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const cardInfo = button.parentNode.querySelector('.card-info');
+        cardInfo.style.display = cardInfo.style.display === 'none' ? 'block' : 'none';
+    });
+});
